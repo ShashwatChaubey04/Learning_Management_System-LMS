e2e1b3287ff4a9f3c399691224cb644517a99e23
Shashwat Chaubey <chaubeyshashwat04062005@gmail.com>
2025-10-08 16:28:23 +0530

    LMS

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { Webhook } from 'svix'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express()
 
@@ -11,12 +13,15 @@ await connectDB()
 
 // Connection of Backend with Other Domain
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
   res.send("Hello World!")
 })
 
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
