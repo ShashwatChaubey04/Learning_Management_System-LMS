78e18d7d574f19b089b438fe55530ed1786ca057
Shashwat Chaubey <chaubeyshashwat04062005@gmail.com>
2025-11-05 09:52:37 +0530

    LMS

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 import courseRouter from './routes/courseRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
 
 
 const PORT = process.env.PORT || 5000
